61f6f88a9b9d14b3d8e79449db82583cba34b3da
Timo Stollenwerk <tisto@plone.org>
2018-12-14 21:08:53 +0100

    Python 3 fixes.

--- src/collective/faq/content.py
+++ src/collective/faq/content.py
@@ -1,8 +1,9 @@
 # -*- coding: utf-8 -*-
 from .interfaces import IHomepage
 from plone.dexterity.content import Container
-from zope.interface import implements
+from zope.interface import implementer
 
 
+@implementer(IHomepage)
 class Homepage(Container):
-    implements(IHomepage)
+    """"""
--- src/collective/faq/tests/test_setup.py
+++ src/collective/faq/tests/test_setup.py
@@ -1,7 +1,14 @@
 # -*- coding: utf-8 -*-
 """Setup tests for this package."""
 from collective.faq.testing import COLLECTIVEFAQ_CORE_INTEGRATION_TESTING  # noqa
-from plone import api
+# from plone import api
+
+try:
+    from Products.CMFPlone.utils import get_installer
+except ImportError:  # Plone < 5.1
+    HAS_INSTALLER = False
+else:
+    HAS_INSTALLER = True
 
 import unittest
 
@@ -14,12 +21,17 @@ class TestSetup(unittest.TestCase):
     def setUp(self):
         """Custom shared utility setup for tests."""
         self.portal = self.layer['portal']
-        self.installer = api.portal.get_tool('portal_quickinstaller')
+        # self.installer = api.portal.get_tool('portal_quickinstaller')
+        self.installer = get_installer(self.portal)
 
     def test_product_installed(self):
         """Test if collective.faq is installed."""
-        self.assertTrue(self.installer.isProductInstalled(
-            'collective.faq'))
+        self.assertTrue(
+            # self.installer.isProductInstalled(
+            #     'collective.faq'
+            # )
+            self.installer.is_product_installed('collective.faq')
+        )
 
     def test_browserlayer(self):
         """Test that ICollectivefaqCoreLayer is registered."""
@@ -35,13 +47,16 @@ class TestUninstall(unittest.TestCase):
 
     def setUp(self):
         self.portal = self.layer['portal']
-        self.installer = api.portal.get_tool('portal_quickinstaller')
-        self.installer.uninstallProducts(['collective.faq'])
+        self.installer = get_installer(self.portal)
+        # self.installer = api.portal.get_tool('portal_quickinstaller')
+        # self.installer.uninstallProducts(['collective.faq'])
+        self.installer.uninstall_product('collective.faq')
 
     def test_product_uninstalled(self):
         """Test if collective.faq is cleanly uninstalled."""
-        self.assertFalse(self.installer.isProductInstalled(
-            'collective.faq'))
+        self.assertFalse(
+            self.installer.is_product_installed('collective.faq')
+        )
 
     def test_browserlayer_removed(self):
         """Test that ICollectivefaqCoreLayer is removed."""
