80878d595091177c812ad65b125ef135d124b84c
Rodrigo Ferreira de Souza <rodfersou@gmail.com>
2019-01-02 19:54:48 -0200

    Add translations

--- src/collective/faq/locales/update.sh
+++ src/collective/faq/locales/update.sh
@@ -6,7 +6,7 @@
 # locales/[language]/LC_MESSAGES/collectivefaq.po
 # (e.g. locales/de/LC_MESSAGES/collectivefaq.po)
 
-domain=collectivefaq
+domain=collective.faq
 
 i18ndude rebuild-pot --pot $domain.pot --create $domain ../
 i18ndude sync --pot $domain.pot */LC_MESSAGES/$domain.po
