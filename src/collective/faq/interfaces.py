45b32b3887d3b1783977dd9065e86333a5d26b74
Rodrigo Ferreira de Souza <rodfersou@gmail.com>
2019-01-02 11:19:06 -0200

    Remove Dublin Core behavior for FAQ Item

--- src/collective/faq/interfaces.py
+++ src/collective/faq/interfaces.py
@@ -1,6 +1,9 @@
 # -*- coding: utf-8 -*-
 """Module where all interfaces, events and exceptions live."""
+from collective.faq import _
+from plone.app.textfield import RichText
 from plone.supermodel import model
+from zope import schema
 from zope.interface import Interface
 from zope.publisher.interfaces.browser import IDefaultBrowserLayer
 
@@ -15,3 +18,15 @@ class IFAQ(model.Schema):
 
 class IFAQItem(Interface):
     """FAQ Item content type interface."""
+
+    title = schema.TextLine(
+        title=_(u'Question'),
+        description=u'The FAQ question.',
+        required=False,
+    )
+
+    answer = RichText(
+        title=_(u'Answer'),
+        description=u'The FAQ question answer.',
+        required=False,
+    )
