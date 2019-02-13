6cbd3192a1e697e2dbb2e270f4dc42b6997907e2
Timo Stollenwerk <stollenwerk@kitconcept.com>
2019-02-13 14:46:19 +0100

    Preparing release 1.0.0a10
    [ci skip]

--- CHANGES.rst
+++ CHANGES.rst
@@ -2,7 +2,7 @@ Changelog
 =========
 
 
-1.0.0a10 (unreleased)
+1.0.0a10 (2019-02-13)
 ---------------------
 
 - Add extra standard behaviors.
--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ long_description = '\n\n'.join([
 
 setup(
     name='collective.faq',
-    version='1.0.0a10.dev0',
+    version='1.0.0a10',
     description='Plone addon package for managing FAQ sections',
     long_description=long_description,
     # Get more from https://pypi.python.org/pypi?%3Aaction=list_classifiers
